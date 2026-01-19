72991ab293eb6c0819293e4a41295d183754ea5e
Philbert <philbertmaluu96@gmail.com>
2026-01-19 11:08:57 -0800

    first commit

---
+++ dpc.cpp
@@ -0,0 +1,10 @@
+<iostream.h>
+int main(){
+    //declaration of variables//
+    int a,b,c,sum=0;
+    cout<<"enter the three numbers";
+    cin>>a,b,c;
+    sum=a+b+c;
+    cout<<"the sum is"<<sum;
+    return 0;
+}
